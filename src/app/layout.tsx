2ff2854ad31e270207f5995e564a5af464a53482
Allie Zhang <admin@thebrotherie.com>
2025-07-21 11:35:38 -0400

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Script from 'next/script'
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
